e24481f02f897fe875182bdc4076bc8d0736d27d
wartave <vatuta12@gmail.com>
2023-11-13 14:34:39 -0400

    update web
    update web

--- main.tf
+++ main.tf
@@ -18,22 +18,25 @@ resource "digitalocean_droplet" "app" {
   size     = "s-1vcpu-1gb"
   ssh_keys = [file("${path.module}/ssh.txt")]
 
-
-  user_data = <<-EOF
-    #cloud-config
-    runcmd:
-      - set -o errexit
-      - sudo apt-get update -y
-      - sudo apt-get install git -y
-      - sudo apt-get install docker.io -y
-      - sudo systemctl start docker
-      - sudo usermod -aG docker root
-      - sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-      - sudo chmod +x /usr/local/bin/docker-compose
-      - git clone https://github.com/wartave/laravel-apache
-      - cd /home/root/app/
-      - sudo chmod 666 /var/run/docker.sock
-      - echo "Ha finalizado"
-  EOF
+   connection {
+    type     = "ssh"
+     user     = "root"
+    private_key = file("~/.ssh/id_rsa")
+    host     = self.ipv4_address  
+  }
+ provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update -y"
+      "sudo apt-get install git -y"
+      "sudo apt-get install docker.io -y"
+      "sudo systemctl start docker"
+      "sudo usermod -aG docker root"
+      "sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose"
+      "sudo chmod +x /usr/local/bin/docker-compose"
+      "git clone https://github.com/wartave/Terraform-jenkins-main /home/root/app/"
+      "cd /home/root/app/"
+      "docker-compose up -d"
+    ]
+  }
   
 }
